c592dd36baa8b7e3e632879f900e5f90d7761b3f
Georg Osang <geoo89@gmail.com>
2021-11-20 22:14:45 +0100

    support for start velocities and goal areas

--- ipe_to_json.py
+++ ipe_to_json.py
@@ -26,24 +26,61 @@ props = {
 	"mark/disk(sx)":   {"is_fixed": True,  "is_player": False},
 }
 
+def str_to_pos(string):
+	return np.array([float(coord) for coord in string.split()[:2]])
+
+def apply_transformation(element, pos):
+	if element.get("matrix") is not None:
+		transformation = [float(coord) for coord in element["matrix"].split()]
+		matrix = np.array(transformation[:4]).reshape(2, 2)
+		shift = np.array(transformation[4:])
+		pos = matrix@pos + shift
+	return np.array(pos)*2
+
 def ipe_to_json(filename, max_y):
+	orb_list = []
+	goals = []
+	vectors = dict()  # start pos to direction
+
 	soup = BeautifulSoup(open(filename, 'r').read(), features="html.parser")
 	page = soup.find('page')
+	paths = page.findAll('path', attrs={'stroke' : 'black'})
+	for path in paths:
+		coords = path.get_text().strip().split('\n')
+		if path.get('arrow') == "normal/normal" and len(coords) == 2:
+			origin = apply_transformation(path, str_to_pos(coords[0]))
+			destination = apply_transformation(path, str_to_pos(coords[1]))
+			direction = destination - origin
+			vectors[tuple(origin)] = direction
+		elif len(coords) == 5 and coords[4] == 'h':
+			# TODO: Add some sanity checking
+			pts = np.array([apply_transformation(path, str_to_pos(pos)) for pos in coords[:4]])
+			rect = [
+				np.min(pts[:,0]),
+				max_y - np.max(pts[:,1]),
+				np.max(pts[:,0]) - np.min(pts[:,0]),
+				np.max(pts[:,1]) - np.min(pts[:,1]),
+			]
+			goals.append(rect)
+
 	nodes = page.findAll('use', attrs={'name' : re.compile(r'^mark/.*')})
-	orb_list = []
 	for node in nodes:
-		pos = np.array([float(coord) for coord in node["pos"].split()])
-		if node.get("matrix") is not None:
-			transformation = [float(coord) for coord in node["matrix"].split()]
-			matrix = np.array(transformation[:4]).reshape(2, 2)
-			shift = np.array(transformation[4:])
-			pos = matrix@pos + shift
-		pos *= 2
+		pos = str_to_pos(node["pos"])
+		pos = apply_transformation(node, pos)
+		vec = vectors.get(tuple(pos), np.array([0.0, 0.0]))
+		vec *= 0.5
+		vec[1] = -vec[1]
 		pos[1] = max_y - pos[1]
-		orb = dict({
+		orb = {
 			"pos" : pos,
 			"charge" : charges[node["stroke"]], 
-			"mass" : sizes[node["size"]], 
-		}, **props[node["name"]])
+			"mass" : sizes[node["size"]],
+			"velocity" : vec,
+		}
+		orb.update(props[node["name"]])
 		orb_list.append(orb)
-	return orb_list
+
+	return {
+		'orbs': orb_list,
+		'goals': goals,
+	}
--- main.py
+++ main.py
@@ -10,40 +10,48 @@ class Orb:
     sprite_image = None
 
     def from_json(obj):
-        return Orb(obj["pos"], obj["charge"], obj["mass"], obj["is_fixed"], obj["is_player"])
+        return Orb(obj["pos"], obj["charge"], obj["mass"], obj["is_fixed"], obj["is_player"], obj["velocity"])
 
     def __init__(self, pos, charge, mass=1, is_fixed=True, is_player=False, velocity=(0.0, 0.0)):
         self.pos = np.array(pos, dtype=float)
         self.charge = float(charge)
         self.mass = float(mass)
-        self.velocity = np.array(velocity)
+        self.velocity = np.array(velocity, dtype=float)
         self.is_fixed = is_fixed
         self.is_player = is_player
 
     def render(self, surface):
         if self.charge < 0:
-            rect = Rect(0, 0, 64, 64)
+            rect = Rect(0, 0, 64, 64)  # blue
         else:
-            rect = Rect(64, 0, 64, 64)
-        pos = (int(self.pos[0]), int(self.pos[1]))
+            rect = Rect(64, 0, 64, 64)  # red
+        pos = (int(self.pos[0]-32), int(self.pos[1]-32))
         surface.blit(Orb.sprite_image, pos, rect)
 
 
 class Physics:
     COLOUMB_CONSTANT = 4000
 
-    def from_json(orb_list, FPS):
+    def from_json(json, FPS):
+        goals = json["orbs"]
         phy = Physics(FPS)
-        for orb in orb_list:
+        for orb in json["orbs"]:
             phy.add_orb(Orb.from_json(orb))
+        for goal in json["goals"]:
+            phy.add_goal(Rect(*list(goal)))
         return phy
 
     def __init__(self, FPS):
         self.FPS = FPS
         self.orbs = []
+        self.goals = []
+        self.win = False
 
     def add_orb(self, orb):
         self.orbs.append(orb)
+
+    def add_goal(self, goal):
+        self.goals.append(goal)
 
     def flip_player_polarity(self):
         for orb in self.orbs:
@@ -51,6 +59,8 @@ class Physics:
                 orb.charge *= -1
 
     def render(self, surface):
+        for goal in self.goals:
+            surface.fill((0, 96, 0), goal)
         for orb in self.orbs:
             orb.render(surface)
 
@@ -70,6 +80,11 @@ class Physics:
         for orb in self.orbs:
             if not orb.is_fixed:
                 orb.pos += orb.velocity / self.FPS
+        for orb in self.orbs:
+            if orb.is_player:
+                for goal in self.goals:
+                    if goal.collidepoint(orb.pos):
+                        self.win = True
 
 
 class App:
@@ -132,6 +147,8 @@ class App:
 
     def on_loop(self):
         self.physics.simulate()
+        if self.physics.win:
+            self.load_next_level()
 
     def on_render(self):
         self._display_surf.fill((0, 0, 0))
